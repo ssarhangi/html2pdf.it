5117eb88ad6ef606f7b3050cfbc364cb8b876124
Sal Sarhangi <sal@sal-pc>
2014-10-30 14:20:55 -0700

    Signed-off-by: Sal Sarhangi <sal@sal-pc>

--- config/production.app.config.js
+++ config/production.app.config.js
@@ -1,5 +1,8 @@
 module.exports ={
 	http:{
-		port: process.env.PORT || process.env.VCAP_APP_PORT || 8080
-	}
+		//port: process.env.PORT || process.env.VCAP_APP_PORT || 8080
+		port: process.env.PORT || 8080
+	}, 
+	//baseUrl: '/html2pdf'
+	baseUrl: '/'
 };
